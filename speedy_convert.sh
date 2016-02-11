03b315e74a6e50b7295d357a0070af49cbda2d6a
Calvin Ha <tbbee24@gmail.com>
2016-02-10 20:53:43 -0800

    Fix date format error - occured on double digit months
    Add function to check for valid url (files)
    
    Downloads now also stores powerpoints in a separate folder

--- speedy_convert.sh
+++ speedy_convert.sh
@@ -59,7 +59,6 @@ function read_file(){
         #substring to store the key and check it if it matches
         key_check="${line:0:${#key_code}}"              
         directory_temp="${line:${#key_code}+1:${#line}}"
-        
         if [ "$key_code" == "$key_check" ] ; then
             directory=$directory_temp #set the directory
             isSet=1
@@ -98,7 +97,26 @@ function check_file(){
 }
 
 #== FUNCTION =====================================================
-#         NAME: get_lectures
+#        NAME: check_url
+#
+# DESCRIPTION: Checks if the url is valid
+#
+# PARAMETER 1: url to check
+#
+#=================================================================
+
+function check_url() {
+    url="$1"
+    # check if the url is valid
+    if ! curl --output /dev/null --silent --head --fail "$url"; then
+        echo "File: $url is unavailable for download"
+        echo "Check if the argument inputted is in the correct format"
+        exit 0
+    fi
+}
+
+#== FUNCTION =====================================================
+#         NAME: get_lecture
 #
 #  DESCRIPTION: Uses wget to download the lecture from the webpage
 #               uses unoconv to convert the pptx to a pdf file
@@ -109,30 +127,32 @@ function check_file(){
 #
 #=================================================================
 
-function get_lectures(){
+function get_lecture(){
     date="$1"
     file_type=".pptx"
     class_year_date="CS$class_number-$YEAR$date"
     
     url=$(printf '%s%s%s%s' "$domain$class_number" "/lectures/" "$class_year_date" "$file_type")
+    check_url $url
 
-    month="${date:1:1}"
-
+    month="${date:0:2}"
     day="${date:2:${#date}}"
     date_format="-$month-$day"
-    
     new_file_name="$class_number$date_format$file_type"
 
     wget -O $new_file_name $url
     unoconv -f pdf $new_file_name
 
-    powerpoint_directory="${class_number}_lectures_powerpoints"
+    powerpoint_directory="${class_number}_lecture_powerpoints"
+    if [ ! -d $powerpoint_directory ]; then
+        mkdir $powerpoint_directory
+    fi
     mv $new_file_name $powerpoint_directory
     exit 0 
 }
 
 #== FUNCTION =====================================================
-#         NAME: get_lectures
+#         NAME: get_assignment
 #
 #  DESCRIPTION: Uses wget to download the assignment from the
 #               webpage
@@ -141,18 +161,18 @@ function get_lectures(){
 #
 #=================================================================
 
-function get_assignments(){
+function get_assignment(){
 
     assignment_num="$1"
     file_type=".pdf"
     assignment="Assignment$assignment_num"
     
     url=$(printf '%s%s%s%s%s' "$domain$class_number" "/assignments/" "$assignment_num/" "$assignment" "$file_type")
+    check_url $url
 
     new_file_name="${class_number}-assignment$1$file_type"
     wget -O $new_file_name $url
     exit 0
-
 }
 
 
@@ -165,11 +185,9 @@ else
     key="$1 $2" # the key is represented by the class number and option 
     if [ "$option" == "-l" ] ;then
         check_file "$key"
-        get_lectures "$3" #Passing in the date to the function 
+        get_lecture "$3" #Passing in the date to the function 
     elif [ "$option" == "-a" ]; then
         check_file "$key"
-        get_assignments "$3" #Passing in the assignment number to the function 
+        get_assignment "$3" #Passing in the assignment number to the function 
     fi
 fi
-    
-
